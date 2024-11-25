fee028bf85d4ee4ce61e2569ef884a90528fa891
re-takurousui <t.usui@re-d.jp>
2024-11-25 11:30:56 +0900

    aaaaa

--- kadai04/script.js
+++ kadai04/script.js
@@ -66,8 +66,7 @@ class ThreeApp {
   count = 2;
   line;
   points = [];
-  tubeCount = 9;
-  // tubes = [];
+  tubeCount = 19;
   tube;
   tubeGeometry;
   material;
@@ -75,6 +74,7 @@ class ThreeApp {
   slider01;
   slider02;
   slider03;
+  aboutSlider;
   isAnimation = false;//アニメーション許可
   isWaiting = false;//待ち時間
   sliders;
@@ -181,18 +181,38 @@ class ThreeApp {
       0xf4f4f7, //グレー
       0xf08300, //オレンジ
       0x25589b, //ブルー
-      0xf4f4f7  //グレー
+      0xf4f4f7,  //グレー
+      0xf08300, //オレンジ
+      0x25589b, //ブルー
+      0xf4f4f7,  //グレー
+      0x25589b, //ブルー
+      0xf08300, //オレンジ
+      0xf4f4f7,  //グレー
+      0x25589b, //ブルー
+      0xf08300, //オレンジ
+      0x25589b, //ブルー
+      0xf4f4f7,  //グレー
     ];
     const tubePositions = [
-      {x: -27, y: 7, z: 2.0},
+      {x: -27, y: 7, z: 2.0},//
       {x: -45, y: -4, z: -5.0},
       {x: -15, y: 10, z: -12.0},
-      {x: -27, y: 7, z: 2.0},
+      {x: -8, y: 11, z: 7.0},//
+      {x: -30, y: -10, z: -15.0},
+      {x: -15, y: 10, z: -15.0},
+      {x: -27, y: 7, z: 2.0},//
       {x: -45, y: -4, z: -5.0},
       {x: -15, y: 10, z: -12.0},
-      {x: -27, y: 7, z: 2.0},
-      {x: -45, y: -4, z: -5.0},
-      {x: -15, y: 10, z: -12.0},
+      {x: -50, y: 15, z: -46.0},//about
+      {x: 20, y: 10, z: -45.0},
+      {x: -20, y: 17, z: -45.0},//com
+      {x: -40, y: 20, z: -45.0},
+      {x: 50, y: 20, z: -35.0},
+      {x: -20, y: 20, z: -45.0},
+      {x: -50, y: -3, z: -49.0},
+      {x: 50, y: 5, z: -49.0},
+      {x: -31, y: 20, z: -45.0},
+      {x: 7, y: 18, z: -45.0},
     ];
     const tubeRotates = [
       {x: 0, y: -0.4, z: -0.15},
@@ -204,14 +224,36 @@ class ThreeApp {
       {x: 0, y: -0.4, z: -0.15},
       {x: 0, y: -0.4, z: 0.0},
       {x: 0, y: -0.4, z: -0.2},
+      {x: 3.7, y: 0.4, z: 0.3},//about
+      {x: 0, y: 3.5, z: 4.0},
+      {x: 0, y: 3, z: 4.5},//com
+      {x: 0, y: 3, z: 3.5},
+      {x: 0, y: 3, z: 6.0},
+      {x: 0, y: 3, z: 4.6},
+      {x: 3.0, y: 0.3, z: 0.0},
+      {x: -6.0, y: 3.4, z: 6.3},
+      {x: 3.3, y: 0.3, z: 1.6},
+      {x: -6.0, y: 3.5, z: 5.0},
     ];
 
     this.slider01 = new THREE.Group();
     this.slider02 = new THREE.Group();
     this.slider03 = new THREE.Group();
+    this.aboutSlider = new THREE.Group();
+    this.companySlider = new THREE.Group();
+    this.serviceSlider = new THREE.Group();
+    this.recruitSlider = new THREE.Group();
+    this.contactSlider01 = new THREE.Group();
+    this.contactSlider02 = new THREE.Group();
     this.scene.add(this.slider01);
     this.scene.add(this.slider02);
     this.scene.add(this.slider03);
+    this.scene.add(this.aboutSlider);
+    this.scene.add(this.companySlider);
+    this.scene.add(this.serviceSlider);
+    this.scene.add(this.recruitSlider);
+    this.scene.add(this.contactSlider01);
+    this.scene.add(this.contactSlider02);
     
     for (let i = 0; i < this.tubeCount; i++) {
       this.material = new THREE.MeshPhongMaterial({ color: tubeColors[i] });
@@ -225,108 +267,152 @@ class ThreeApp {
         this.slider02.add(this.tube);
       } else if (i >= 6 && i <= 8) { // 7~9
         this.slider03.add(this.tube);
+      } else if (i >= 9 && i <= 10) { // 10~11
+        this.aboutSlider.add(this.tube);
+      } else if (i == 11) { // 10~11
+        this.companySlider.add(this.tube);
+      } else if (i == 12) { // 10~11
+        this.serviceSlider.add(this.tube);
+      } else if (i >= 13 && i <= 14) { // 10~11
+        this.recruitSlider.add(this.tube);
+      } else if (i >= 15 && i <= 16) { // 10~11
+        this.contactSlider01.add(this.tube);
+      } else if (i >= 17 && i <= 18) { // 10~11
+        this.contactSlider02.add(this.tube);
       }
     }
     this.slider02.position.x = 10;
     this.slider02.rotation.y = 10;
     this.slider03.position.x = 0.7;
     this.slider03.rotation.z = -1.5;
-
-    // const observer = new IntersectionObserver((entries) => {
-    //   entries.forEach((entry) => {
-    //     if(entry.isIntersecting) {
-    //       // gsap.to(mat, {
-    //       //   opacity: 0,
-    //       //   duration: 2, // 2秒かけて変化
-    //       //   onComplete: () => {
-    //       //     console.log('アニメーション終了');
-    //       //   },
-    //       // });
-    //       gsap.to(this.slider01.children, {
-    //         opacity: 0,
-    //         duration: 2, // アニメーションの時間（秒）
-    //         onStart: () => {
-    //           // 透明度を制御可能にするため、すべてのマテリアルのtransparentをtrueに設定
-    //           this.slider01.children.forEach((child) => {
-    //             if (child.material) {
-    //               child.material.transparent = true;
-    //             }
-    //           });
-    //         },
-    //         onComplete: () => {
-    //           // アニメーション完了後に非表示
-    //           this.slider01.visible = true;
-    //         },
-    //       });
-    //       console.log('表');
-    //     } else {
-    //       console.log('消える');
-    //       gsap.to(this.slider01.children, {
-    //         opacity: 0,
-    //         duration: 2, // アニメーションの時間（秒）
-    //         onStart: () => {
-    //           // 透明度を制御可能にするため、すべてのマテリアルのtransparentをtrueに設定
-    //           this.slider01.children.forEach((child) => {
-    //             if (child.material) {
-    //               child.material.transparent = true;
-    //             }
-    //           });
-    //         },
-    //         onComplete: () => {
-    //           // アニメーション完了後に非表示
-    //           this.slider01.visible = false;
-    //         },
-    //       });
-    //     }
-    //   });
-    // }, {
-    //   rootMargin: '0% 0% 0% 0%',
-    // });
-    // observer.observe(document.querySelector('.fv'));
+    gsap.to(this.slider01.position, {
+      z: 60, 
+      scrollTrigger: {
+        trigger: ".fv",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+    gsap.to(this.slider02.position, {
+      z: 60,
+      scrollTrigger: {
+        trigger: ".fv",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+    gsap.to(this.slider03.position, {
+      z: 60,
+      scrollTrigger: {
+        trigger: ".fv",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+    gsap.to(this.aboutSlider.position, {
+      z: 70,
+      scrollTrigger: {
+        trigger: ".about",
+        start: "top 80%",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+    gsap.to(this.companySlider.position, {
+      z: 70,
+      scrollTrigger: {
+        trigger: ".company",
+        start: "top 100%",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+    gsap.to(this.serviceSlider.position, {
+      z: 70,
+      scrollTrigger: {
+        trigger: ".service",
+        start: "top 80%",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+    gsap.to(this.recruitSlider.position, {
+      z: 70,
+      scrollTrigger: {
+        trigger: ".recruit",
+        start: "top 60%",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+    gsap.to(this.contactSlider01.position, {
+      z: 70,
+      scrollTrigger: {
+        trigger: ".contact",
+        start: "top 150%",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+    gsap.to(this.contactSlider02.position, {
+      z: 58,
+      scrollTrigger: {
+        trigger: ".contact",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
   }
   
-  //描画
+  //FV描画
   moveLine() { 
-    if (!this.isAnimation && !this.isWaiting) {
-      // 描画アニメーション
-      this.tube.geometry.setDrawRange(this.start, this.count);
-      setTimeout(() => {
-        this.count += 300;//.0001秒たったら描画スタート
-      },1);
-      if (this.count > this.tubeGeometry.index.count) {
-        // setTimeout(() => {
-          this.isAnimation = true;
-          this.isWaiting = false;
-        // }, 1);
+    const targetTube01 = this.slider01.children;
+    const targetTube02 = this.slider02.children;
+    const targetTube03 = this.slider03.children;
+    if(targetTube01 && targetTube02 && targetTube03) {
+      if (!this.isAnimation && !this.isWaiting) {
+        // 描画アニメーション
+        this.tube.geometry.setDrawRange(this.start, this.count);
+        setTimeout(() => {
+          this.count += 300;
+        },1);
+        if (this.count > this.tubeGeometry.index.count) {
+          // setTimeout(() => {
+            this.isAnimation = true;
+            this.isWaiting = false;
+          // }, 1);
+        }
+      } else if (this.isAnimation) {
+        // 消去アニメーション
+        this.tube.geometry.setDrawRange(this.start, this.count);
+        this.start += 291;
+        if (this.start >= this.tubeGeometry.index.count) {
+          // 消去完了したらリセットして再ループ
+          this.start = 0;
+          this.count = 2;
+          this.isAnimation = false;
+          // 次のスライダーに移行
+          this.currentIndex = (this.currentIndex + 1) % this.sliders.length;
+        }
       }
-    } else if (this.isAnimation) {
-      // 消去アニメーション
-      this.tube.geometry.setDrawRange(this.start, this.count);
-      this.start += 291;
-
-      if (this.start >= this.tubeGeometry.index.count) {
-        // 消去完了したらリセットして再ループ
-        this.start = 0;
-        this.count = 2;
-        this.isAnimation = false;
-        // 次のスライダーに移行
-        this.currentIndex = (this.currentIndex + 1) % this.sliders.length;
-        // console.log(this.currentIndex);
-      }
-    }
-    this.sliders = [this.slider01, this.slider02, this.slider03];
-    const addNextSlider = () => {
-      // すべてのスライダーを一度シーンから削除
-      this.sliders.forEach((slider) => this.scene.remove(slider));
+      this.sliders = [this.slider01, this.slider02, this.slider03];
+      const addNextSlider = () => {
+        // すべてのスライダーを一度シーンから削除
+        this.sliders.forEach((slider) => this.scene.remove(slider));
+    
+        // 現在のスライダーをシーンに追加
+        this.scene.add(this.sliders[this.currentIndex]);
   
-      // 現在のスライダーをシーンに追加
-      this.scene.add(this.sliders[this.currentIndex]);
-
-      // 1秒後に次のスライダーを追加
-      // setTimeout(addNextSlider, 1000);
-    };
-    // 初回実行
-    addNextSlider();
+        // 1秒後に次のスライダーを追加
+        // setTimeout(addNextSlider, 1000);
+      };
+      // 初回実行
+      addNextSlider();
+    }
   }
 
   //線を度からラジアン
